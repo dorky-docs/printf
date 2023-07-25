8e5b2ed4cb51e1c398bcb0762a5e592a14268dc1
Vikky779 <olubunmi779@gmail.com>
2023-07-25 12:07:41 -0700

    This is to get the size

---
+++ 1-get_size.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * get_size - Size calculation
+ * @format: String formatted to print argument
+ * @i: Argument list
+ * Return: Size
+ */
+int get_size(const char *format, int *i)
+{
+	int cu_i = *i + 1;
+	int size = 0;
+
+	if (format[cu_i] == 'l')
+		size = S_LONG;
+	else if (format[cu_i] == 'h')
+		size == S_SHORT;
+
+	if (size == 0)
+		*i = cu_i - 1;
+	else
+		*i = cu_i;
+	return (size);
+}
