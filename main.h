4036d0b8317925400d16e80e20a91593344a5e61
Dorcas Fordjour <docfords2017@gmail.com>
2023-07-25 17:28:07 -0700

    This is the first group project in alx where we were tasked to come up with our own prototyps and custom printf function

--- main.h
+++ main.h
@@ -7,21 +7,21 @@
 #define UNUSED(x) (void)(x)
 #define BUFF_SIZE 1024
 
-/* FLAGS */
+/*FLAGS*/
 #define F_MINUS 1
 #define F_PLUS 2
 #define F_ZERO 4
 #define F_HASH 8
 #define F_SPACE 16
 
-/* SIZES */
+/*SIZES*/
 #define S_LONG 2
 #define S_SHORT 1
 
 /**
- *struct fmt - Struct op
- * @fmt: The format.
- * @fn: The function associated.
+ *struct fmt - Struct operand
+ * @fmt: The format
+ * @fn: The associated function
  */
 struct fmt
 {
